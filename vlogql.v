19a3c2b7be02b3e825cae5fcb6c2513fca876da5
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-03-13 11:56:57 +0100

    label handlers

--- vlogql.v
+++ vlogql.v
@@ -29,15 +29,39 @@ struct Values {
 fn fetch_logs(api string, query string, num int) {
         data := http.get_text('${api}/loki/api/v1/query_range?query=${query}&limit=${num}')
         res := json.decode(Response, data) or { exit(1) }
+        println('---------- Logs for: ${query}')
         for row in res.data.result {
+	  // TODO: parse & display labels
           // println('Labels: ${row.stream}')
           for log in row.values {
              println(log[1])
           }
         }
-        println('----------')
         return
 }
+
+struct Labels {
+        status string
+        data []string
+}
+
+fn fetch_labels(api string, label string) {
+     if utf8_str_len(label) > 0 {
+        data := http.get_text('${api}/loki/api/v1/label/${label}/values')
+        res := json.decode(Labels, data) or { exit(1) }
+        println('---------- Values for: ${label}')
+        println(res.data)
+        return
+
+     } else {
+        data := http.get_text('${api}/loki/api/v1/labels')
+        res := json.decode(Labels, data) or { exit(1) }
+        println('---------- Labels:')
+        println(res.data)
+        return
+     }
+}
+
 
 fn main() {
         mut fp := flag.new_flag_parser(os.args)
@@ -48,6 +72,8 @@ fn main() {
         logql_limit := fp.int('limit', `l`, 5, 'number of logs to show')
         logql_api := fp.string('api', `a`, 'http://localhost:3100', 'logql api')
         logql_query := fp.string('query', `q`, '', 'logql query')
+        logql_labels := fp.bool('labels', `t`, false, 'get labels')
+        logql_label := fp.string('label', `v`, '', 'get label values')
 
         fp.finalize() or {
                 eprintln(err)
@@ -55,7 +81,18 @@ fn main() {
                 return
         }
 
-        println('Fetching logs...')
-        fetch_logs(logql_api, logql_query, logql_limit)
+	if logql_labels {
+          fetch_labels(logql_api, '')
+	}
+	else if utf8_str_len(logql_label) > 0 {
+          fetch_labels(logql_api, logql_label)
+	}
+	else if utf8_str_len(logql_query) > 0 {
+          println('Fetching logs...')
+          fetch_logs(logql_api, logql_query, logql_limit)
+	} else {
+                println(fp.usage())
+		return
+	}
 
 }
